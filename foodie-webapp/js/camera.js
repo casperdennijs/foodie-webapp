6cba33b62586712a1ed3da76071c6216cde6de15
Casper <casper.dennijs@outlook.com>
2023-02-14 13:28:48 +0100

    Camera functionaliteiten

---
+++ foodie-webapp/js/camera.js
@@ -0,0 +1,18 @@
+const scanButton = document.querySelector(".scanner")
+const scanSection = document.querySelector("section.scannen")
+const video = document.querySelector("#video")
+console.log(scanButton)
+
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  const constraints = { 
+    video: true,
+    audio: false
+  }
+  
+  navigator.mediaDevices.getUserMedia(constraints).then(stream => video.srcObject = stream);
+}
+
+scanButton.addEventListener('click', () => {
+    console.log("test");
+    scanSection.classList.toggle("enable");
+})
