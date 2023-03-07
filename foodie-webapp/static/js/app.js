164e88ca56cb468169f8cd1047155c7e07105e24
Casper <casper.dennijs@outlook.com>
2023-03-07 12:41:04 +0100

    Small updates

--- foodie-webapp/static/js/app.js
+++ foodie-webapp/static/js/app.js
@@ -1,10 +1,12 @@
 import { router } from "./modules/router.js";
 import { searchResults } from "./modules/search.js";
 import { previousPage, nextPage } from "./modules/pagination.js";
+import { createVideo } from "./modules/camera.js";
 
 const searchForm = document.querySelector("form");
 const previousButton = document.querySelector("#previous");
 const nextButton = document.querySelector("#next");
+const scanButton = document.querySelector(".scanner")
 
 if (window.location.hash == "") {
     window.location.hash = "#home"
@@ -15,3 +17,4 @@ router();
 searchForm.addEventListener("submit", searchResults);
 previousButton.addEventListener("click", previousPage);
 nextButton.addEventListener("click", nextPage);
+scanButton.addEventListener('click', createVideo)
